a5bab0aff0b4c842cb31147763f05549c07e6ab9
Nathan Ojuolape <ojuolapeolabode@meta.com>
2024-12-12 09:40:34 +0000

    thursday 12th  commit

--- tests/headerNavigations.spec.ts
+++ tests/headerNavigations.spec.ts
@@ -10,7 +10,7 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('Verify the get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
@@ -30,29 +30,22 @@ test(`Verify Navigation to Docs`, async ({ page }) => {
   await expect(page).toHaveURL(/.*docs/);
 
   // Verify the presence of a key element on the Docs page.
-  // const docsHeader = await page.locator("h1");
-  // await expect(docsHeader).toHaveText("Installation");
+  const docsHeader = await page.locator("h1");
+  await expect(docsHeader).toHaveText("Installation");
 
-  // Assert the header text on the Docs page
-  // await docsPage.assertHeaderText("h1", "Installation");
-
-  // // Assert the presence of other key elements on the Docs page
-  // await docsPage.assertElementPresent("h2", "Getting Started");
-  // await docsPage.assertElementPresent("h2", "API Reference");
-  // await docsPage.assertElementPresent("h2", "Contributing");
-  // await docsPage.assertElementPresent("h2", "FAQ");
-  // await docsPage.assertElementPresent("h2", "Changelog");
 });
+
 
 test(`Verify Navigation to Home`, async ({ page }) => {
   const docsPage = new DocsPage(page);; // Initialize the HomePage object
 
-    // // Navigate to the Home page
+   // Navigate to the Home page
     await docsPage.navigateTo('');
 
     // Verify the URL contains '/playwright.dev'
     await expect(page).toHaveURL(/.*playwright.dev/);
   });
+
 
   test(`Verify Navigation to API page`, async ({ page }) => {
     const docsPage = new DocsPage(page);; // Initialize the HomePage object
